5cf74436a0b8e69977f8e838e9a09af5bbff5c6d
Juan Ospina <juan.ospinal@outlook.com>
2023-08-13 22:23:41 -0500

    Initial config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hola Mundo");
+const express = require('express');
+
+// Initialize express app
+const app = express();
+
+
+// Routes configuration
+app.get('/', (req, res) => {
+  res.status(200).json({
+    ok: true,
+    message: 'Welcome to the api of calendar app'
+  });
+})
+
+app.listen(4000, () => {
+  console.log(`listening on port 4000`);
+});
